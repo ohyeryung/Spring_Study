55f83ec32d14d9b123428659394f0d9d1acd8286
ohyeryung <dpfu1948@g.eulji.ac.kr>
2023-01-23 19:45:07 +0900

    :white_check_mark: solved Bj10171

---
+++ src/main/java/com/example/algorithm/basic/Bj10171.java
@@ -0,0 +1,10 @@
+package com.example.algorithm.basic;
+
+public class Bj10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
