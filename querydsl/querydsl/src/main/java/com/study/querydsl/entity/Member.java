0231caf7e75fa3370d4239ddb3760fb8c5df8c83
ohyeryung <dpfu1948@g.eulji.ac.kr>
2023-04-01 18:38:02 +0900

    querydsl study

---
+++ querydsl/querydsl/src/main/java/com/study/querydsl/entity/Member.java
@@ -0,0 +1,48 @@
+package com.study.querydsl.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+// @ToString 사용 시 연관관계가 맺어있는 필드는 가급적 피하기
+@ToString(of = {"id", "username", "age"})
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this(username, 0);
+    }
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+}
---
+++ querydsl/querydsl/src/main/java/com/study/querydsl/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.study.querydsl.repository;
+
+import com.study.querydsl.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom  {
+    List<Member> findByUsername(String username);
+}
