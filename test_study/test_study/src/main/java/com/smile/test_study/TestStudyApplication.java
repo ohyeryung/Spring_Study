8a2268cdc8849d6eb84ce77e2a589457ebe4282d
ohyeryung <dpfu1948@g.eulji.ac.kr>
2023-04-19 18:51:23 +0900

    feat. JUnit 5 활용한 테스트 코드 공부

---
+++ test_study/test_study/src/main/java/com/smile/test_study/TestStudyApplication.java
@@ -0,0 +1,13 @@
+package com.smile.test_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestStudyApplication.class, args);
+    }
+
+}
