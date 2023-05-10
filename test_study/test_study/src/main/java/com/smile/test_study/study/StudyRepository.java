bda39f4311eba43fd3b8ee65d7ac5583b7e1d31d
ohyeryung <dpfu1948@g.eulji.ac.kr>
2023-05-10 17:37:14 +0900

    refactor. test_study 폴더구조 변경

--- test_study/test_study/src/main/java/com/smile/test_study/study/StudyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smile.test_study.study;
-
-import com.smile.test_study.domian.Study;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudyRepository extends JpaRepository<Study, Long> {
-}
--- test_study/test_study/src/main/java/com/smile/test_study/study/StudyStatus.java
+++
@@ -1,5 +0,0 @@
-package com.smile.test_study.study;
-
-public enum StudyStatus {
-    DRAFT, OPENED, END
-}
