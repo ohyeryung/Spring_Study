2a602518c40122fdc4100d9428803f9e491da6b3
ohyeryung <dpfu1948@g.eulji.ac.kr>
2023-05-11 17:12:42 +0900

    refactor. querydsl 폴더구조 변경

--- querydsl/src/main/java/com/study/querydsl/entity/Team.java
+++ querydsl/src/main/java/com/study/querydsl/entity/Team.java
@@ -21,7 +21,7 @@ public class Team {
     private String name;
 
     @OneToMany(mappedBy = "team")
-    private List<Member> members = new ArrayList<>();
+    private final List<Member> members = new ArrayList<>();
 
     public Team(String name) {
         this.name = name;
