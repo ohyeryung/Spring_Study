9fdcf24317f14d96daff2c5e0b2bd14f249dddeb
ohyeryung <dpfu1948@g.eulji.ac.kr>
2023-05-27 01:51:15 +0900

    feat. chaos-monkey 사용 설정 추가

---
+++ test_study/src/main/java/com/smile/test_study/study/StudyController.java
@@ -0,0 +1,24 @@
+package com.smile.test_study.study;
+
+import com.smile.test_study.domian.Study;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class StudyController {
+
+    final StudyRepository repository;
+
+    @GetMapping("/study/{id}")
+    public Study getStudy(@PathVariable Long id) {
+        return repository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Study not found for '" + id + "'")
+        );
+    }
+
+    @PostMapping("/study")
+    public Study createStudy(@RequestBody Study study) {
+        return repository.save(study);
+    }
+}
