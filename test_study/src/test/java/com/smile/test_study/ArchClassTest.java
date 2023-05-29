657d7b3a1162d586f1a8f48a108771ed8e12eef1
ohyeryung <dpfu1948@g.eulji.ac.kr>
2023-05-29 23:54:28 +0900

    feat. ArchUnit 클래스 의존성 test

---
+++ test_study/src/test/java/com/smile/test_study/ArchClassTest.java
@@ -0,0 +1,30 @@
+package com.smile.test_study;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import javax.persistence.Entity;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+@AnalyzeClasses(packagesOf = TestStudyApplication.class)
+public class ArchClassTest {
+
+    @ArchTest
+    ArchRule controllerClassRule = classes().that().haveSimpleNameEndingWith("Controller")
+            .should().accessClassesThat().haveSimpleNameEndingWith("Service")
+            .orShould().accessClassesThat().haveSimpleNameEndingWith("Repository");
+
+    @ArchTest
+    ArchRule repositoryClassRule = noClasses().that().haveSimpleNameEndingWith("Repository")
+            .should().accessClassesThat().haveSimpleNameEndingWith("Service");
+
+    @ArchTest
+    ArchRule studyClassesRule = classes().that().haveSimpleNameStartingWith("study")
+            .and().areNotEnums()
+            .and().areNotAnnotatedWith(Entity.class)
+            .should().resideInAnyPackage("..study..");
+}
+
--- test_study/src/test/java/com/smile/test_study/ArchTests.java
+++ test_study/src/test/java/com/smile/test_study/ArchTests.java
@@ -1,47 +1,31 @@
 package com.smile.test_study;
 
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import org.junit.jupiter.api.Test;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
+@AnalyzeClasses(packagesOf = TestStudyApplication.class)
 public class ArchTests {
 
-    @Test
-    void packageDependencyTests() {
-        JavaClasses classes = new ClassFileImporter().importPackages("com.smile.test_study");
+    @ArchTest
+    ArchRule domainPackageRule = classes().that().resideInAPackage("..domain..")
+            .should().onlyBeAccessed().byClassesThat()
+            .resideInAnyPackage("..study..", "..member..", "..domain..");
 
-                /*
-                 * TODO ..domain.. 패키지에 있는 클래스는 ..study.., ..member.., ..domain에서 참조 가능.
-                 */
-                ArchRule domainPackageRule = classes().that().resideInAPackage("..domain..")
-                .should().onlyBeAccessed().byClassesThat()
-                .resideInAnyPackage("..study..", "..member..", "..domain..");
-                domainPackageRule.check(classes);
+    @ArchTest
+    ArchRule memberPackageRule = noClasses().that().resideInAnyPackage("..domain..")
+            .should().accessClassesThat().resideInAPackage("..member..");
 
-                /*
-                 * TODO (반대로) ..domain.. 패키지는 ..member.. 패키지를 참조하지 못한다.
-                 */
-                ArchRule memberPackageRule = noClasses().that().resideInAnyPackage("..domain..")
-                .should().accessClassesThat().resideInAPackage("..member..");
-                memberPackageRule.check(classes );
+    @ArchTest
+    ArchRule studyPackageRule = noClasses().that().resideOutsideOfPackage("..study..")
+            .should().accessClassesThat().resideInAPackage("..study..");
 
-                /*
-                 * TODO ..study.. 패키지에 있는 클래스는 ..study.. 에서만 참조 가능.
-                 */
-                ArchRule studyPackageRule = noClasses().that().resideOutsideOfPackage("..study..")
-                .should().accessClassesThat().resideInAPackage("..study..");
-                studyPackageRule.check(classes );
+    @ArchTest
+    ArchRule freeOfCycles = slices().matching("..test_study.(*)..")
+            .should().beFreeOfCycles();
 
-                /*
-                 * TODO 순환 참조가 없어야 한다.
-                 */
-                ArchRule freeOfCycles = slices().matching("..test_study.(*)..")
-                        .should().beFreeOfCycles();
-                        freeOfCycles.check(classes);
-    }
 }
