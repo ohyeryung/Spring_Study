8146be98ecb11d9e8bc28825883d77ce70759ba1
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-02-08 22:56:26 +0900

    :white_check_mark: solved Bj10171

---
+++ algorithm/src/Bj10171.java
@@ -0,0 +1,8 @@
+public class Bj10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
