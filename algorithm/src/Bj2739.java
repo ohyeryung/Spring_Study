366dcef37245f5d865e37ff88245d1ed23f938e3
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-02-13 17:39:25 +0900

    :white_check_mark: solved Bj2739

---
+++ algorithm/src/Bj2739.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj2739 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i < 10; i ++) {
+            System.out.println(num + " * " + i + " = " + num*i);
+        }
+    }
+}
