bc630e0b704b800d8fe219160486ef788c445c45
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-02-14 00:22:18 +0900

    :white_check_mark: solved Bj11022

---
+++ algorithm/src/repetition/Bj11022.java
@@ -0,0 +1,25 @@
+package repetition;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj11022 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= T; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append("Case #").append(i).append(": ").append(a).append(" + ").append(b).append(" = ").append(a + b);
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
