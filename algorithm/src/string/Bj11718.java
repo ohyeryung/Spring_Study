1eaee4da772b32cf30c1d4858d50ad936f153982
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-02-26 19:56:44 +0900

    :white_check_mark: solved Bj11718

---
+++ algorithm/src/string/Bj11718.java
@@ -0,0 +1,16 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+        System.exit(0);
+    }
+}
