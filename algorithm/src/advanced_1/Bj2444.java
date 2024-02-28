eb7037dde514068cbdcb2452d5bb3e6155369fa7
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-02-28 14:59:22 +0900

    :white_check_mark: solved Bj2444

---
+++ algorithm/src/advanced_1/Bj2444.java
@@ -0,0 +1,32 @@
+package advanced_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj2444 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < N-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = N-1; i >= 1; i--) {
+            for (int j = 0; j < N-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
