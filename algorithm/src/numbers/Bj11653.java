47973db78e299d9cdc67b1ac584b3f4571d29116
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-03-22 15:45:38 +0900

    :white_check_mark: solved Bj11653

---
+++ algorithm/src/numbers/Bj11653.java
@@ -0,0 +1,22 @@
+package numbers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj11653 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i*i <= N; i++) {
+            while (N%i == 0) {
+                System.out.println(i);
+                N /= i;
+            }
+        }
+        if (N > 1) {
+            System.out.println(N);
+        }
+    }
+}
