906d1c00db07c599c4477512f6589fb3c33161e1
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-03-24 12:13:08 +0900

    :white_check_mark: solved Bj5073

---
+++ algorithm/src/geometry/Bj5073.java
@@ -0,0 +1,37 @@
+package geometry;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Bj5073 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        while (true) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (a == 0) break;
+            int[] arr = new int[3];
+            arr[0] = a;
+            arr[1] = b;
+            arr[2] = c;
+            Arrays.sort(arr);
+
+            if (arr[2] >= arr[0] + arr[1]) {
+                System.out.println("Invalid");
+            } else if (a == b && b == c) {
+                System.out.println("Equilateral");
+            } else if (a == b || b == c || c == a) {
+                System.out.println("Isosceles");
+            } else {
+                System.out.println("Scalene");
+            }
+        }
+    }
+}
