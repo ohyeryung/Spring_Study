94bd6bfc40e7c00cdc7ff578b14ec5e533638d7f
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-04-01 02:04:08 +0900

    :white_check_mark: solved Bj25305

---
+++ algorithm/src/sort/Bj25305.java
@@ -0,0 +1,27 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Bj25305 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            arr[i] = x;
+        }
+        Arrays.sort(arr);
+
+        System.out.println(arr[N-k]);
+
+    }
+}
