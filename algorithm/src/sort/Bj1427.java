a25aad6cbe3fa3b7c6871e4d66a82d50b8821629
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-04-01 15:42:06 +0900

    :zap: improved Bj1427

--- algorithm/src/sort/Bj1427.java
+++ algorithm/src/sort/Bj1427.java
@@ -9,9 +9,9 @@ public class Bj1427 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String N = br.readLine();
-        int[] arr = new int[N.length()];
+        Character[] arr = new Character[N.length()];
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = N.charAt(i) - '0';
+            arr[i] = N.charAt(i);
         }
         Arrays.sort(arr);
         for (int i = arr.length - 1; i >= 0; i--) {
