4b9e286013069ea5c5cfac8d0e985b57ac78e639
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-04-03 15:31:03 +0900

    :white_check_mark: solved Bj11650

---
+++ algorithm/src/sort/Bj11650.java
@@ -0,0 +1,41 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Bj11650 {
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        arr = new int [N][2];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (arr_1, arr_2) -> {
+
+            if(arr_1[0] == arr_2[0])  {
+                return arr_1[1] - arr_2[1];
+            }
+            else {
+                return arr_1[0] - arr_2[0];
+            }
+        });
+
+        for(int i=0; i<N; i++) {
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
