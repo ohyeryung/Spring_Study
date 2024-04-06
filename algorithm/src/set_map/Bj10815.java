5d4c8d6f6e49ba87b7cd124f0ddc4bffb726bee9
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-04-06 10:00:23 +0900

    :white_check_mark: solved Bj10815

---
+++ algorithm/src/set_map/Bj10815.java
@@ -0,0 +1,35 @@
+package set_map;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Bj10815 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < M; i++) {
+            if (set.contains(Integer.parseInt(st1.nextToken()))) {
+                sb.append(1).append(" ");
+            } else {
+                sb.append(0).append(" ");
+            }
+        }
+        System.out.println(sb);
+    }
+}
