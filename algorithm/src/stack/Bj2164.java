c71c3ca6c032ec9eb4867c4ce75cf696cc19a77c
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-04-17 16:55:16 +0900

    :white_check_mark: solved Bj2164

---
+++ algorithm/src/stack/Bj2164.java
@@ -0,0 +1,28 @@
+package stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Bj2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            if ((queue.peek()) % 2 == 0) {
+                queue.offer(queue.peek());
+                queue.poll();
+            }
+        }
+        System.out.println(queue.element());
+    }
+}
